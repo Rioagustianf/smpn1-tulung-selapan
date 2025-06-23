660852e133ac5453a03842eedd017d9a9abdf42d
Rio Agustian <rioagustian2001@gmail.com>
2025-06-23 23:56:30 +0700

    feat: menambahkan fitur sitemape

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://smpn1tulungselapan.cloud", // Ganti dengan domain kamu
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
